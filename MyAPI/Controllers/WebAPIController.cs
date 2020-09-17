85b13bcf28c8a5306b6a70be9e69aff55102e9d0
raushanmahmud <raushanm.re@gmail.com>
2020-09-17 06:39:50 +0500

    added WebAPIController, wrote demo logic

---
+++ MyAPI/Controllers/WebAPIController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyAPI.Controllers
+{
+    public class WebAPIController : ApiController
+    {
+        public string Get()
+        {
+            return "Welcome to the Web api";
+        }
+
+        public List <string> Get(int id)
+        {
+            return new List<string>
+            {
+                "Data 1",
+                "Data 2"
+            };
+        }
+    }
+}
